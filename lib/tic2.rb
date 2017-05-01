79caa6b7b6bb0fdc729d4c5dc49bc8ea511e4b2c
icebear <wilsontan.2088@gmail.com>
2017-05-01 22:50:16 +0800

    Add gameplay class

--- lib/tic2.rb
+++ lib/tic2.rb
@@ -47,7 +47,8 @@ class Position
   def minimax
     # these are the base cases when game terminate with win, lose or draw
     # assuming x to be the computer, this would assign highest point to x winning
-    puts "#{@@minimax}" if defined? @@minimax
+    # puts "#{@@minimax}" if defined? @@minimax
+    # binding.pry
     return 100 if win("x")
     return -100 if win("o")
     return 0 if possible_moves.empty?
@@ -63,7 +64,7 @@ class Position
     # Given this result, "o" will choose index 7 and this is done by method: send(change_turn(:max, :min)) which seeks to maximise
     # or minimise the return value depending on whose turn it is. As such, this board position will have a point of -100
     @@minimax[@board] = possible_moves.map { |possible_space|
-      puts "whose turn: #{turn}\nmove: #{possible_space}\npoints: #{move(possible_space).minimax}"
+      # puts "whose turn: #{turn}\nmove: #{possible_space}\npoints: #{move(possible_space).minimax}"
       move(possible_space).minimax
     }.send(change_turn(:max, :min))
   end
@@ -75,3 +76,58 @@ class Position
   end
 
 end
+
+class Game
+  def get_player_input(position)
+    while true
+      print "Enter position 1-9: "
+      user_input = gets.to_i - 1
+      return user_input if position.possible_moves.include?(user_input)
+    end
+  end
+
+  def render_board(board)
+    puts "#{board[0].gsub(/-/, " ")} | #{board[1].gsub(/-/, " ")} | #{board[2].gsub(/-/, " ")}"
+    puts "------------"
+    puts "#{board[3].gsub(/-/, " ")} | #{board[4].gsub(/-/, " ")} | #{board[5].gsub(/-/, " ")}"
+    puts "------------"
+    puts "#{board[6].gsub(/-/, " ")} | #{board[7].gsub(/-/, " ")} | #{board[8].gsub(/-/, " ")}"
+  end
+
+  def play_game
+    @position = Position.new(nil,"o")
+    while true
+      player_move = get_player_input(@position)
+      @position = @position.move(player_move)
+      
+      if @position.possible_moves.empty?
+        puts "no more move - draw"
+        render_board(@position.board)
+        break
+      else
+      end
+
+      computer_move = @position.best_move
+      @position = @position.move(computer_move)
+      render_board(@position.board)
+
+      if @position.win("o")
+        puts "you win"
+        render_board(@position.board)
+        break
+      elsif @position.win("x")
+        puts "computer win"
+        render_board(@position.board)
+        break
+      elsif @position.possible_moves.empty?
+        puts "no more move - draw"
+        render_board(@position.board)
+        break
+      else
+      end
+
+    end
+  end
+end
+
+Game.new.play_game
